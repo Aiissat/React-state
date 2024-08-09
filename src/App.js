136bcbfa43ad5dbfcb2eb6cf1205d11d58e7783a
Diallo <m.aiissatoudiallo@gmail.com>
2024-08-09 18:01:15 +0000

    reac-state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import meImage from './me.jpeg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayState = (this.state.show)? 'flex' : 'none';
+  }
+  state = {
+   Person: {
+      fullName: "Aïssatou Diallo",
+      bio: "I am a student in GoMyCode as an junior software developer",
+      imgSrc: meImage,
+      profession: "Student"
+    },
+    show: true,
+   interval: null,
+   elapsedTime: 0,
+  };
+
+  changeShowState = () => {
+    (this.state.show)? this.setState({show: false}) : this.setState({show: true});
+  }
+  //Reglage d'une intervalle qui incremenete l'heure chaque seconde
+componentDidMount() {
+    const interval = setInterval(() => {this.setState({ elapsedTime: this.state.elapsedTime + 1 })}, 1000);
+    this.setState({ interval });
+  }
+
+  //L'intervalle est effacé  a chaque fois qu'on raffraichit la page
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
+
+  render() {
+    this.displayState = (this.state.show)? 'flex' : 'none';
+
+    return (
+      <div style={{width: '500px'}}>
+        <div className='App'>
+          <div style={{display: this.displayState,flexDirection: 'column', justifyContent: 'space-around'}}>
+            <img src = {this.state.Person.imgSrc} alt="presentation" style={{borderRadius: '10%', width: '400px' , height: '500px' , margin: 'auto'}}></img> 
+            <h1 style={{margin: '20px' , fontSize:'50px' }}>{this.state.Person.fullName}</h1>
+            <h2 style={{margin: '6px' , fontSize:'30px' }}>{this.state.Person.profession}</h2>
+            <p style = {{textAlign: 'justify' , textIndent: '40px', color: 'black' , margin: '6px'}}>{this.state.Person.bio}</p>
+          </div>
+          <button onClick= {this.changeShowState} style={{borderRaduis: '10px' , padding:'10px' , border: 'none', backgroundColor:'orange' , color:'black', cursor: 'pointer'}}>Change View</button>
+        </div>
+        <div className = "intervalwatcher">
+          <label htmlFor="intervalDisplay">Time elapsed since the component was last mounted: <br/><br/> <input readOnly id = "intervalDisplay" value = {this.state.elapsedTime} type="text"></input></label>
+
+        </div>
+      </div>
+    );
+  }
+
 }
-
 export default App;
